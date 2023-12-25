17b056744b25b277d5d5fb78138ce0579f8ecd4b
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-25 14:22:02 +0300

    created Item model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return self.name
+
