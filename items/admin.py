fd8843a2c4b33f16e55756ccfe1aacc6c7369367
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-25 16:01:13 +0300

    register Item model in Django admin

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from items.models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    fields = ['name', 'description', 'price']
+    search_fields = ['name']
+    ordering = ['-pk']
+
