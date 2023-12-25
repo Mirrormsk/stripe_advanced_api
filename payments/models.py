b768e7e108ef805fde2230d62430eeb338630f78
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-25 15:48:05 +0300

    Created models Order, Tax and Discount

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from items.models import Item
+
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Discount(models.Model):
+    percentage = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='процент скидки')
+
+    class Meta:
+        verbose_name = 'скидка'
+        verbose_name_plural = 'скидки'
+
+
+class Tax(models.Model):
+    rate = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='размер')
+
+    class Meta:
+        verbose_name = 'налог'
+        verbose_name_plural = 'налоги'
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, **NULLABLE)
+    tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, **NULLABLE)
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
