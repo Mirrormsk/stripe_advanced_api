0a0a81ff4bda6fa0d7e51ece84eb8122c5b3f61c
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 10:44:30 +0300

    Added fields to Tax model

--- payments/models.py
+++ payments/models.py
@@ -2,20 +2,29 @@ from django.db import models
 
 from items.models import Item
 
-
 NULLABLE = {'null': True, 'blank': True}
 
 
 class Discount(models.Model):
-    percentage = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='процент скидки')
+    percent_off = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='процент скидки', default=0)
+    amount_off = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='сумма скидки', default=0)
+    currency = models.CharField(max_length=3, **NULLABLE, verbose_name='валюта')
 
     class Meta:
         verbose_name = 'скидка'
         verbose_name_plural = 'скидки'
 
+    def __str__(self):
+        return (f'{self.amount_off if self.amount_off else ''} '
+                f'{self.currency if self.currency else ''}'
+                f'{str(self.percent_off) + '%' if self.percent_off else ""}')
+
 
 class Tax(models.Model):
-    rate = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='размер')
+    percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='размер')
+    display_name = models.CharField(max_length=20, verbose_name='название')
+    inclusive = models.BooleanField(verbose_name='включен')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
 
     class Meta:
         verbose_name = 'налог'
@@ -25,7 +34,7 @@ class Tax(models.Model):
 class Order(models.Model):
     items = models.ManyToManyField(Item)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='создан')
-    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, **NULLABLE)
+    discounts = models.ManyToManyField(Discount)
     tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, **NULLABLE)
 
     class Meta:
