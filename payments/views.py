027eaad8cf836a66f911bc1b8f16dfb76e69c76a
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:02:29 +0300

    created views for Order

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from payments.models import Order
+from payments.serializers import OrderSerializer
+from payments.services import StripeApiClient
+
+
+class OrderCreateAPIView(CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderPaymentView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderListAPIView(ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderPaymentAPIView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request, *args, **kwargs):
+        order = get_object_or_404(Order, pk=kwargs.get('pk'))
+        session = StripeApiClient.execute_payment(order)
+        return Response({'order_id': order.id, "session_id": session.id, 'checkout_url': session.url},
+                        status=status.HTTP_200_OK)
