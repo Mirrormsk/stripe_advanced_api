c7301feca1d52af52f7547976a1ce03eb801f91b
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:06:54 +0300

    code formatting

--- items/admin.py
+++ items/admin.py
@@ -5,8 +5,7 @@ from items.models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price']
-    fields = ['name', 'description', 'price']
-    search_fields = ['name']
-    ordering = ['-pk']
-
+    list_display = ["name", "price"]
+    fields = ["name", "description", "price"]
+    search_fields = ["name"]
+    ordering = ["-pk"]
