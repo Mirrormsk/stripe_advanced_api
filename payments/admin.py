ec78b1eab0588078047fbc012994f365606ad992
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 20:59:49 +0300

    field tax renamed to tax_rates in Order

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from items.models import Item
+from payments.models import Order, Discount, Tax
+
+
+class ItemSetInline(admin.TabularInline):
+    model = Item
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['created_at']
+    fields = ['items',
+              'discounts',
+              'tax_rates']
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+    fields = ['amount_off', 'currency', 'percent_off']
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ['percentage',
+                    'display_name',
+                    'inclusive']
+    fields = ['percentage',
+              'display_name',
+              'inclusive',
+              'description']
+    search_fields = ['display_name']
+
