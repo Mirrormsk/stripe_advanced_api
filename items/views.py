1879cd88e4df5d3533bfdabfa3bc6890605e42f3
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:02:25 +0300

    created views for item detail and item buy

--- items/views.py
+++ items/views.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.views.generic import DetailView
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from items.models import Item
+from items.serializers import ItemSerializer
+from payments.services import StripeApiClient
+
+
+class ItemDetailView(DetailView):
+    model = Item
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['STRIPE_PUBLIC_KEY'] = settings.STRIPE_PUBLIC_KEY
+        return context_data
+
+
+class ItemBuyView(RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def get(self, request, *args, **kwargs):
+        item = self.get_object()
+        session = StripeApiClient.execute_payment(item)
+        print(session)
+        return Response({'item_id': item.id, "session_id": session.id}, status=status.HTTP_200_OK)
