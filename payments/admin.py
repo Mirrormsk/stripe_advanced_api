5db3117c8ae1f88b35e9765258b46b6bc2750bcc
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:32:28 +0300

    optimized admin models

--- payments/admin.py
+++ payments/admin.py
@@ -4,13 +4,11 @@ from payments.models import Order, Discount, Tax
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["created_at"]
     fields = ["items", "discounts", "tax_rates"]
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
-    list_display = ["__str__"]
     fields = ["amount_off", "currency", "percent_off"]
 
 
