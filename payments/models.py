a363ea5b0a3695440c9ca0b658bb6e1dfb820f02
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:09:00 +0300

    code formatting

--- payments/models.py
+++ payments/models.py
@@ -2,41 +2,49 @@ from django.db import models
 
 from items.models import Item
 
-NULLABLE = {'null': True, 'blank': True}
+NULLABLE = {"null": True, "blank": True}
 
 
 class Discount(models.Model):
-    percent_off = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='процент скидки', default=0)
-    amount_off = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='сумма скидки', default=0)
-    currency = models.CharField(max_length=3, **NULLABLE, verbose_name='валюта')
+    percent_off = models.DecimalField(
+        max_digits=6, decimal_places=2, verbose_name="процент скидки", default=0
+    )
+    amount_off = models.DecimalField(
+        max_digits=6, decimal_places=2, verbose_name="сумма скидки", default=0
+    )
+    currency = models.CharField(max_length=3, **NULLABLE, verbose_name="валюта")
 
     class Meta:
-        verbose_name = 'скидка'
-        verbose_name_plural = 'скидки'
+        verbose_name = "скидка"
+        verbose_name_plural = "скидки"
 
     def __str__(self):
-        return (f'{self.amount_off if self.amount_off else ''} '
-                f'{self.currency if self.currency else ''}'
-                f'{str(self.percent_off) + '%' if self.percent_off else ""}')
+        return (
+            f'{self.amount_off if self.amount_off else ""} '
+            f'{self.currency if self.currency else ""}'
+            f'{str(self.percent_off) + "%" if self.percent_off else ""}'
+        )
 
 
 class Tax(models.Model):
-    percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='размер')
-    display_name = models.CharField(max_length=20, verbose_name='название')
-    inclusive = models.BooleanField(verbose_name='включен')
-    description = models.TextField(verbose_name='описание', **NULLABLE)
+    percentage = models.DecimalField(
+        max_digits=5, decimal_places=2, verbose_name="размер"
+    )
+    display_name = models.CharField(max_length=20, verbose_name="название")
+    inclusive = models.BooleanField(verbose_name="включен")
+    description = models.TextField(verbose_name="описание", **NULLABLE)
 
     class Meta:
-        verbose_name = 'налог'
-        verbose_name_plural = 'налоги'
+        verbose_name = "налог"
+        verbose_name_plural = "налоги"
 
 
 class Order(models.Model):
     items = models.ManyToManyField(Item)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="создан")
     discounts = models.ManyToManyField(Discount)
     tax_rates = models.ManyToManyField(Tax)
 
     class Meta:
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        verbose_name = "заказ"
+        verbose_name_plural = "заказы"
