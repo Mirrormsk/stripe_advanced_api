ef2ce8d24f169e1dc607fcb1d1fe8482a01cffe7
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-26 21:09:21 +0300

    code formatting

--- payments/admin.py
+++ payments/admin.py
@@ -4,26 +4,18 @@ from payments.models import Order, Discount, Tax
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['created_at']
-    fields = ['items',
-              'discounts',
-              'tax_rates']
+    list_display = ["created_at"]
+    fields = ["items", "discounts", "tax_rates"]
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
-    list_display = ['__str__']
-    fields = ['amount_off', 'currency', 'percent_off']
+    list_display = ["__str__"]
+    fields = ["amount_off", "currency", "percent_off"]
 
 
 @admin.register(Tax)
 class TaxAdmin(admin.ModelAdmin):
-    list_display = ['percentage',
-                    'display_name',
-                    'inclusive']
-    fields = ['percentage',
-              'display_name',
-              'inclusive',
-              'description']
-    search_fields = ['display_name']
-
+    list_display = ["percentage", "display_name", "inclusive"]
+    fields = ["percentage", "display_name", "inclusive", "description"]
+    search_fields = ["display_name"]
