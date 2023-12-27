a47a2821fdcd02558a8ad5a07845cb991ed74874
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-27 16:34:16 +0300

    using different api keys for USD and EUR payments

--- payments/services.py
+++ payments/services.py
@@ -1,31 +1,51 @@
+import collections
+
 import environ
 import stripe
 from stripe.checkout import Session
 
 from items.models import Item
 from payments.models import Order, Discount, Tax
+from django.conf import settings
 
 env = environ.Env(DEBUG=(bool, False))
 
-stripe.api_key = env("STRIPE_API_KEY")
+stripe.api_key = settings.STRIPE_DEFAULT_SECRET_KEY
 SUCCESS_URL = env("PAYMENT_SUCCESS_URL")
-DEFAULT_PAYMENT_QUANTITY = env("DEFAULT_PAYMENT_QUANTITY")
 
 
 class StripeApiClient:
     """Stripe API Client class"""
 
     @staticmethod
-    def create_product(name: str, description: str) -> stripe.Product:
-        product = stripe.Product.create(name=name, description=description)
+    def get_stripe_api_key(currency):
+        if currency == "USD":
+            return settings.STRIPE_SECRET_KEY_USD
+        elif currency == "EUR":
+            return settings.STRIPE_SECRET_KEY_EUR
+        else:
+            return settings.STRIPE_DEFAULT_SECRET_KEY
+
+    @staticmethod
+    def get_stripe_public_key(currency):
+        if currency == "USD":
+            return settings.STRIPE_PUBLIC_KEY_USD
+        elif currency == "EUR":
+            return settings.STRIPE_PUBLIC_KEY_EUR
+        else:
+            return settings.STRIPE_DEFAULT_PUBLIC_KEY
+
+    @classmethod
+    def create_product(cls, item: Item) -> stripe.Product:
+        product = stripe.Product.create(name=item.name, description=item.description)
         return product
 
     @classmethod
     def create_price(cls, item: Item) -> stripe.Price:
-        product = cls.create_product(item.name, item.description)
+        product = cls.create_product(item)
 
         price = stripe.Price.create(
-            unit_amount_decimal=str(item.price),
+            unit_amount_decimal=str(item.price * 100),
             currency=item.currency,
             product=product.id,
         )
@@ -33,7 +53,7 @@ class StripeApiClient:
 
     @staticmethod
     def create_coupon(discount: Discount) -> stripe.Coupon:
-        """Creates a Stripe coupon """
+        """Creates a Stripe coupon"""
         coupon = stripe.Coupon.create(
             amount_off=discount.amount_off if discount.amount_off else None,
             currency=discount.currency if discount.currency else None,
@@ -49,19 +69,27 @@ class StripeApiClient:
             description=tax.description if tax.description else None,
             percentage=tax.percentage,
             inclusive=tax.inclusive,
-            country='FR',
+            country=tax.country,
         )
         return stripe_tax
 
     @classmethod
     def get_line_items_from_order(cls, order: Order):
         tax_rates = [cls.create_tax_rate(tax).id for tax in order.tax_rates.all()]
-        line_items = [{"price": cls.create_price(item).id, "quantity": 1, "tax_rates": tax_rates} for item in order.items.all()]
+        line_items = [
+            {"price": cls.create_price(item).id, "quantity": 1, "tax_rates": tax_rates}
+            for item in order.items.all()
+        ]
         return line_items
 
     @classmethod
     def create_session(cls, order: Order) -> Session:
-        """Creates a Stripe Session Object"""
+        """Creates a Stripe Session Object.
+        Uses different api keys for EUR and USD currencies
+        """
+        currency = order.items.first().currency
+        stripe.api_key = cls.get_stripe_api_key(currency)
+
         line_items = cls.get_line_items_from_order(order)
         coupons = [cls.create_coupon(discount).id for discount in order.discounts.all()]
 
@@ -75,9 +103,7 @@ class StripeApiClient:
         return session
 
     @classmethod
-    def execute_payment(
-            cls, payment_object: Order | Item
-    ) -> Session:
+    def execute_payment(cls, payment_object: Order | Item) -> Session:
 
         if isinstance(payment_object, Item):
             order = Order.objects.create()
@@ -86,7 +112,7 @@ class StripeApiClient:
             payment_object = order
 
         elif not isinstance(payment_object, Order):
-            raise TypeError('Only Order and Item objects are supported')
+            raise TypeError("Only Order and Item objects are supported")
 
         session = cls.create_session(payment_object)
 
@@ -96,8 +122,6 @@ class StripeApiClient:
     def retrieve_session(session_id: str) -> Session:
         session = stripe.checkout.Session.retrieve(session_id)
         return session
-
-
 
     # @staticmethod
     # def create_payment_intent(order: Order):
@@ -110,7 +134,9 @@ class StripeApiClient:
     #         metadata={'order_id': order.id},
     #     )
 
+
 class PaymentService:
     """Payment service"""
+
     def create_payment(self, order: Order) -> dict:
         ...
