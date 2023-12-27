659691597d107b8644b1309ecfe35690b01f7c5e
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-27 18:48:34 +0300

    added custom description for responses for payment views

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.response import Response
@@ -13,11 +15,6 @@ class OrderCreateAPIView(CreateAPIView):
     serializer_class = OrderSerializer
 
 
-class OrderPaymentView(APIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-
-
 class OrderListAPIView(ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
@@ -29,6 +26,20 @@ class OrderPaymentAPIView(APIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "order_id": openapi.Schema(type=openapi.TYPE_INTEGER),
+                    "session_id": openapi.Schema(type=openapi.TYPE_STRING),
+                    "checkout_url": openapi.Schema(type=openapi.TYPE_STRING),
+                },
+                required=["order_id", "payment_intent", "client_secret"],
+            ),
+            404: "Not Found",
+        },
+    )
     def get(self, request, *args, **kwargs):
         order = get_object_or_404(Order, pk=kwargs.get("pk"))
         session = StripeApiClient.execute_payment(order)
@@ -48,13 +59,27 @@ class OrderPaymentIntentAPIView(APIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "order_id": openapi.Schema(type=openapi.TYPE_INTEGER),
+                    "payment_intent_id": openapi.Schema(type=openapi.TYPE_STRING),
+                    "client_secret": openapi.Schema(type=openapi.TYPE_STRING),
+                },
+                required=["order_id", "payment_intent", "client_secret"],
+            ),
+            404: "Not Found",
+        },
+    )
     def get(self, request, *args, **kwargs):
         order = get_object_or_404(Order, pk=kwargs.get("pk"))
         payment_intent = StripeApiClient.execute_payment_intent(order)
         return Response(
             {
                 "order_id": order.id,
-                "payment_intent": payment_intent.id,
+                "payment_intent_id": payment_intent.id,
                 "client_secret": payment_intent.client_secret,
             },
             status=status.HTTP_200_OK,
