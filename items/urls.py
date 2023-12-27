5507647e2308e6f14ee88b4cdbb19b5a7388343d
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-27 19:57:21 +0300

    added url for list View for items

--- items/urls.py
+++ items/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from items.apps import ItemsConfig
-from items.views import ItemDetailView, ItemBuyView
+from items.views import ItemDetailView, ItemBuyView, ItemListView
 
 app_name = ItemsConfig.name
 
 urlpatterns = [
+    path('', ItemListView.as_view(), name='item-list'),
     path('item/<int:pk>/', ItemDetailView.as_view(), name='item_detail'),
     path('buy/<int:pk>/', ItemBuyView.as_view(), name='item_buy'),
 ]
