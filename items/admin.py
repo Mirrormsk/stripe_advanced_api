1d5249de2b64412a22506a419788a4dd40e15a82
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-27 16:35:06 +0300

    add field id to list_display for Item admin

--- items/admin.py
+++ items/admin.py
@@ -5,7 +5,7 @@ from items.models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ["name", "price"]
-    fields = ["name", "description", "price"]
+    list_display = ["id", "name", "price", "currency"]
+    fields = ["name", "description", "price", "currency"]
     search_fields = ["name"]
     ordering = ["-pk"]
