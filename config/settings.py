a87684bcc548f3f9cd6fb61567d61ebbaa5a6150
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-28 17:29:10 +0300

    add static root

--- config/settings.py
+++ config/settings.py
@@ -97,13 +97,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 PAYMENT_SUCCESS_URL = env("PAYMENT_SUCCESS_URL")
